62d7eab83fcfd3913b543f0e1b61758273e2fc6f
joaovictor2025 <joaovictor03062012@gmail.com>
2025-04-25 20:07:31 -0300

    Terceira aula de IA na EDN

---
+++ estudo.py
@@ -0,0 +1,25 @@
+print("Acordei")
+print("Ir ao Banheiro")
+print("Tomar Café")
+print("Escovar os dentes")
+
+
+# Definicção Clara 
+print("Vamos Somar 2 Números,  me diga qual e digite")
+numero1 = int(input("Digite o Primeiro NUmero:"))
+numero2 = int(input("Digite o Segundo NUmero:"))
+soma = numero1 + numero2
+print("A soma é: ", soma)
+
+# finitude
+
+'''for i in range(5):
+    print("Contar",i)
+while True:
+    print("Não tem fim")'''
+
+
+# determinismo
+def soma(a, b):
+    return a + b
+print(soma (4,5))
